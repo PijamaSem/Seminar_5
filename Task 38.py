891928a7ceec42e72dc44972179688d375e28be8
Your Name <you@example.com>
2023-01-18 16:42:16 +0300

    Task38

--- Task 38.py
+++ Task 38.py
@@ -2,4 +2,10 @@
 # Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
 
 data = 'абв вба абвгд ыбва фываыб'
+bad_text='абв'
 print('Исходный данные:',data)
+new_data = []
+for word in data.split():
+    if bad_text not in word:
+        new_data.append(word)
+print(f"Слова из текста не содержащие '{bad_text}':",new_data)
